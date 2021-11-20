6fd341cfb6d9bba9cf581e3eff32cebe689b1f5f
ftr9 <ferlandodotel16@gmail.com>
2021-11-20 07:27:21 +0545

    added info

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,18 @@ import './Header.css'
 const Header = () => {
   return (
     <div className="Header">
-      <img src={'./assets/logo.png'} alt={'logo'} />
-      <div className="Header__button">Admin</div>
+      <img
+        className={'HeaderLogo__main'}
+        src={'./assets/logo.png'}
+        alt={'logo'}
+      />
+      <a
+        href={'https://www.facebook.com/profile.php?id=100074356586764'}
+        className="Header__button"
+      >
+        <p>Our page</p>
+        <img src={'/assets/logo.png'} alt={'logo'} />
+      </a>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,34 +1,23 @@
 import './Footer.css'
-import mapboxgl from 'mapbox-gl'
-import { useEffect } from 'react'
-
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiZnRyOSIsImEiOiJja3MyM3NsdjExenI5Mm5uODlteGxhbXdvIn0.0AkcaTfC9swqHJgqbr8TLg'
-
 const Footer = () => {
-  useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: 'map',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [85.324, 27.7172],
-      zoom: 12,
-    })
-
-    new mapboxgl.Marker({ color: 'red' })
-      .setLngLat([85.324, 27.7172])
-      .addTo(map)
-  }, [])
-
   return (
     <div className="footer">
-      <div className="footer__About">
-        <div className="footer__About--Title">About</div>
-        <div className="footer__About--secondary">contact:98XXXXXXXX</div>
-        <div className="footer__About--secondary">
-          Pepsicola townplanning ktm 35 pepsicola
-        </div>
+      <div className="footer__info footer__info1">
+        <div className="footer__info-header">CashOut Info</div>
+        <p>Normal Cashout at 50$ (Minimum)</p>
+        <p>
+          If Match Given then redemable at (4X) i.e 4 times the match. ( Match
+          -> 20$ then Redemable at 80$ )
+        </p>
       </div>
-      <div className="Map" id="map"></div>
+      <div className="footer__Stick"></div>
+      <div className="footer__info footer__info2">
+        <div className="footer__info-header">Referal Bonus</div>
+        <p>
+          5$ bonus will be given if the referal plays more than 3 matched of
+          (greater than 50$ total)
+        </p>
+      </div>
     </div>
   )
 }
